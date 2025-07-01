34a61826675732a3607d8624b94f9dfb4f5729cd
Aurgart <ilyalemeshew@gmail.com>
2025-07-01 14:13:18 +0300

    Домашка.

--- src/JavaJabki3Test.java
+++ src/JavaJabki3Test.java
@@ -4,5 +4,62 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class JavaJabki3Test {
-
+    // Задание 1
+    @Test
+    public void sumRangeTest(){
+        Assertions.assertEquals(5, Main.sumRange(new int[] {1, 2, 3}) );
+        Assertions.assertEquals(6, Main.sumRange(new int[] {1, 2, 3}) );
+        Assertions.assertNotNull(Main.sumRange(new int[] {}) );
+    }
+    // Задание 2
+    @Test
+    public void addIntTest(){
+        Assertions.assertEquals(9, Main.add(4, 5) );
+        Assertions.assertEquals(100, Main.add(45, 60) );
+        Assertions.assertEquals(0, Main.add(0, 0) );
+    }
+    @Test
+    public void addStringTest(){
+        Assertions.assertEquals("Hello World", Main.add("Hello  ", "World") );
+        Assertions.assertEquals("Hello World", Main.add("Go to  ", "Hell") );
+        Assertions.assertEquals("", Main.add("", "") );
+    }
+    // Задание 3
+    @Test
+    public void findMaxTest(){
+        Assertions.assertEquals(3, Main.findMax(new int[] {1, 2, 3}) );
+        Assertions.assertEquals(99, Main.sumRange(new int[] {12, -2, 3, 4, 99, -220}) );
+        Assertions.assertNotNull(Main.findMax(new int[] {}) );
+    }
+    // Задание 4
+    @Test
+    public void factorialTest(){
+        Assertions.assertEquals(24, Main.factorial(4) );
+        Assertions.assertEquals(24, Main.factorial(-4) );
+    }
+    // Задание 5
+    @Test
+    public void figureAreaTestCircle(){
+        Assertions.assertEquals(0.06, Main.figureArea(0.14) );
+        // провалится из-за учета округления, в двух знаках это надо учитывать.
+        Assertions.assertEquals(12.56, Main.figureArea(2) );
+    }
+    @Test
+    public void figureAreaTestSquare(){
+        Assertions.assertEquals(20, Main.figureArea(4, 5) );
+        Assertions.assertEquals(24, Main.figureArea(-4, 6) );
+    }
+    // Задание 6
+    @Test
+    public void averageOfThreeNumbersTest(){
+        Assertions.assertEquals(2.5,Main.averageOfThreeNumbers(new double[]{2.5, 4, 1}));
+        // провалится, так как внутри проверка на 3 числа.
+        Assertions.assertEquals(2.5,Main.averageOfThreeNumbers(new double[]{2.5, 4, 3, 0.5}));
+    }
+    // Задание 7
+    @Test
+    public void getHypotenuseTest(){
+        Assertions.assertEquals(3.62, Main.getHypotenuse(1.5, 3.3));
+        Assertions.assertEquals(2.3, Main.getHypotenuse(-1, 9));
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,159 @@ public class Main {
     7. Напишите метод, который принимает два числа, представляющие длины катетов, и возвращает длину гипотенузы (используйте теорему Пифагора).
     8. Реализовать тесты для задач выше :)
     * */
+        System.out.println("Домашка два, погнали.");
+        // Задание 1
+        System.out.println("Задание 1: " + sumRange(new int[]{1, 222, -99, 114}));
+        // Задание 2.1
+        System.out.println("Задание 2.1: " + add(278, -299));
+        // Задание 2.2
+        System.out.println("Задание 2.2: " + add("Аве Цезарю!", "Аве, мне!"));
+        // Задание 3
+        System.out.println("Задание 3: " + findMax(new int[]{11, 212, -799, 1414, 890809, 0}));
+        // Задание 4
+        System.out.println("Задание 4: " + factorial(7));
+        // Задание 5.1
+        System.out.println("Задание 5.1: " + figureArea(0.14));
+        // Задание 5.2
+        System.out.println("Задание 5.2: " + figureArea(13.10, 4));
+        // Задание 6
+        System.out.println("Задание 6: " + averageOfThreeNumbers(new double[]{4.1, 3, 5.9}));
+        // Задание 7
+        System.out.println("Задание 7: " + getHypotenuse(0.5, 2.7));
+    }
+    /**
+     * возвращает сумму целых чисел из массива/списка Задание 1
+     * @param numbers - массив int
+     * @return - сумма четных чисел
+     */
+    static Integer sumRange(int[] numbers){
+        if (numbers.length == 0 ){
+            System.out.println("Массив не должен быть из нуля элементов");
+            return null;
+        }
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++){
+            sum += numbers[i];
+        }
+        return sum;
+    }
+    /**
+     * Возвращает сумму двух целых чисел Задание 2
+     * @param num1
+     * @param num2
+     * @return
+     */
+    static  int add(int num1, int num2){
+        if (num1 == 0 || num2 == 0){
+            System.out.println("А смысл?");
+        }
+        return  num1 + num2;
+    }
+    /**
+     * Возвращает конкатенацию слов Задание 2
+     * @param str1
+     * @param str2
+     * @return
+     */
+    static String add( String  str1, String str2){
+        if (str2 == null || str1 == null){
+            System.out.println("А смысл?");
+        }
+        return  str1 + str2;
+    }
+    /**
+     * Примитивный поиск максимума Задание 3
+     * @param numbers - массив чисел
+     * @return - максимальное
+     */
+    static Integer findMax(int[] numbers){
+        if (numbers == null || numbers.length == 0){
+            return null;
+        }
+        int max  = numbers[0];
+        for (int i = 1; i < numbers.length; i++){
+            max = max > numbers[i] ? max : numbers[i];
+        }
+        return max;
+    }
+    /**
+     * получение факториала числа Задание 4
+     * @param num - целое число.
+     * @return - факториал
+     */
+    static int factorial(int num){
+        if (num < 0){
+            System.out.println("Факториал возможен только по положительным числам, и нулю.");
+            return  0;
+        } else if (num == 0) {
+            return  1;
+        }
+        int fact = num;
+        while(num > 0) {
+            fact = fact * num;
+            num--;
+        }
+        return  fact;
+    }
+    /**
+     *  Расчет площади круга задание 5
+     * @param radius - радиус круга
+     * Добавил тут проверку на if как 5 задание из if-else
+     * */
+    static double figureArea(double radius){
+        if (radius <= 0) {
+            System.out.println("Радиус должен быть больше нуля, если ноль то это точка,а она площади не имеет");
+        }
+        double p =  3.14159;
+        return Double.parseDouble(getFormatUnified(p * (radius * radius)));
+    }
+    /**
+     *  Расчет площади прямоугольника задание 5
+     * @param side1 - первая сторона
+     * @param side2 - вторая сторона
+     * Добавил тут проверку на if как 5 задание из if-else
+     * */
+    static double figureArea(double side1, double side2){
+        if (side1 <= 0 || side2 <= 0) {
+            System.out.println("Стороны должны быть больше нуля");
+        }
+        return Double.parseDouble(getFormatUnified(side1*side2));
+    }
+    /**
+     *  Возвращает арифметическое для 3 чисел Задание 6
+     *  Проверка на то что массив из 3 чисел в коде)
+     * @param numbers - массив типа double
+     * @return
+     */
+    static double averageOfThreeNumbers(double[] numbers){
+        if(numbers.length != 3) {
+            System.out.println("Тут не 3 числа");
+        }
+        return Double.parseDouble(getFormatUnified((numbers[0] + numbers[1]+numbers[2])/numbers.length ));
+    }
+    /**
+     * Получение гипотенузы по 2 катетам.
+     * @param cathetus1
+     * @param cathetus2
+     * @return
+     */
+    static double getHypotenuse(double cathetus1, double cathetus2){
+        if (cathetus1 <= 0 || cathetus2 <=0) {
+            System.out.println("Длинна катетов должна быть больше 0");
+        }
+        return  Double.parseDouble(getFormatUnified(Math.sqrt(Math.pow(cathetus1, 2) + Math.pow(cathetus2, 2))));
+    }
+
+    /**
+     * Функция для приведения к 2 символам
+     * Написана чтобы везде не тащить формат, и автотесты норм отработали.
+     * @param number
+     * @return
+     */
+    static String getFormatUnified(double number){
+        DecimalFormatSymbols dcSymbol = new DecimalFormatSymbols();
+        dcSymbol.setDecimalSeparator('.');
+        DecimalFormat df = new DecimalFormat("0.00", dcSymbol);
+        return df.format(number);
     }
 }
